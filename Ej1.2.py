e2890ef4101920697e52b52216d9807f0d75115a
Negro_13 <141974651+Negro-13@users.noreply.github.com>
2025-03-17 21:26:32 -0300

    Add files via upload

---
+++ Ej1.2.py
@@ -0,0 +1,3 @@
+lista = [{"nombre": "Juan", "edad": 25, "pais": "México"}, {"nombre": "Ana", "edad": 30, "pais": "España"}, {"nombre": "Carlos", "edad": 28, "pais": "Argentina"}]
+print(f'Los keys de los dicionarios son: {list(lista[0].keys())}')
+print(f'Los values del primero son: {list(lista[0].values())}')
---
+++ Ej1.py
@@ -0,0 +1,5 @@
+lista = [{"nombre": "Juan", "edad": 25, "pais": "México"}, {"nombre": "Ana", "edad": 30, "pais": "España"}, {"nombre": "Carlos", "edad": 28, "pais": "Argentina"}]
+
+print(f'El nombre del primero es {lista[0]["nombre"]}')
+print(f'La edad del segundo es {lista[1]["edad"]}')
+print(f'El pais del tercero es {lista[3]["pais"]}')
